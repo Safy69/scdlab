fa0e105f8ee43a75369e47edb6e47a2df717fdcf
Safy69 <72312905+Safy69@users.noreply.github.com>
2022-06-09 03:26:49 -0700

    Add files via upload

---
+++ ExceptionHandling.java
@@ -0,0 +1,28 @@
+package lab12;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ExceptionHandling {
+	public static void main(String args[])
+	{
+		try (Scanner input = new Scanner(System.in)) {
+			{
+				try {
+					int[] array = {9,8,7,6,5,4,3,2,1,0,2,5,8,9,6,3};
+					System.out.println("*************************");
+					System.out.println("Enter Aray Index Number: ");
+					int index = input.nextInt();
+					System.out.println("========================");
+					System.out.println("We Are Having Number: "+array[index]);
+					System.out.println("*************************");}
+				
+				catch(ArrayIndexOutOfBoundsException e) {
+					System.out.println("Array Index Is Out Of Bounds");
+					System.out.println("*************************");
+				}
+			}
+		}
+	}
+}
+
